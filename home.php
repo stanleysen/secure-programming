bfbcd03ac77fa38941e2c706bbe53d7b367a83aa
stanleysen <stanley.riyanto@binus.ac.id>
2024-01-04 10:24:20 +0700

    vuln fix - idor, sql

--- home.php
+++ home.php
@@ -29,9 +29,27 @@ if(isset($_POST['add_to_cart'])){
    if(mysqli_num_rows($check_cart_numbers) > 0){
       $message[] = 'produk sudah ditambahkan!';
    }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'produk berhasil ditambahkan!';
+
+      $user_id = htmlspecialchars($user_Id, ENT_QUOTES, "UTF-8");
+      $product_name = htmlspecialchars($product_name, ENT_QUOTES, 'UTF-8'); 
+      $product_price = htmlspecialchars($product_price, ENT_QUOTES, 'UTF-8');
+      $product_quantity = htmlspecialchars($product_quantity, ENT_QUOTES, 'UTF-8');
+      $product_Image = htmlspecialchars($product_Image, ENT_QUOTES, 'UTF-8');
+
+
+      $insert_cart = mysqli_prepare($conn, "INSERT INTO 'cart' (user_id, name, price, quantity, image) VALUES (?, ?, ?, ?, ?)");
+      mysqli_stmt_bind_param($insert_cart, "issis", $user_id, $product_name, $product_price, $product_quantity, $product_image);
+      mysqli_stmt_execute($insert_cart);
+      
+
    }
+   if(mysqli_stmt_affected_rows($insert_cart) > 0){
+      $message[] = 'Produk berhasil ditambahkan!';
+   } else{
+      $message[] = 'Gagal menambahkan produk!';
+   }
+   mysqli_stmt_close($insert_cart);
+   $message[] = 'Produk berhasil ditambahkan!';
 
 }
 
@@ -76,24 +94,23 @@ if(isset($_POST['add_to_cart'])){
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
+
      <form action="" method="post" class="box">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
       <div class="price">Rp. <?php echo $fetch_products['price']; ?>/-</div>
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
-      <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-      <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-      <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+      <!-- change 1 -->
+      <!-- change 5 -->
+      <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
       <input type="submit" value="Tambahkan ke keranjang" name="add to cart" class="btn">
      </form>
+
       <?php
          }
       }else{
          echo '<p class="empty">Belum ada produk yang terunggah!</p>';
       }
-      
-      
-      
       $conn->close();
       ?>
    </div>
